ff75bb302172b46c50030a7f140a99a298885a43
Binau02 <aubin.saunier@gmail.com>
2023-11-05 00:24:58 -0400

    oui

--- js/edit.js
+++ js/edit.js
@@ -7,3 +7,15 @@ title.className = "editable Title"
 title.innerHTML = "Section titre"
 line.append(title)
 content.append(line)
+
+line = document.createElement("div");
+line.className = "line"
+machine = document.createElement("div")
+machine.className = "editable Machine"
+machine.innerHTML = "Section machine"
+line.append(machine)
+mechanism = document.createElement("div")
+mechanism.className = "editable Mechanism"
+mechanism.innerHTML = "Section mechanism"
+line.append(mechanism)
+content.append(line);
