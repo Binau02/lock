6d568ed9d9f2b5959de57ca87c8bf913d0eb4a8c
Binau02 <aubin.saunier@gmail.com>
2023-11-05 00:10:00 -0400

    oui

--- js/edit.js
+++ js/edit.js
@@ -3,4 +3,7 @@ content = document.getElementById("content");
 line = document.createElement("div");
 line.className = "line"
 title = document.createElement("div");
-title.className = "Editable Title"
+title.className = "editable Title"
+title.innerHTML = "Section titre"
+line.append(title)
+content.append(line)
