402421ae9c5a8bd48924942bd8a4afffd50ae0b3
Louison-Diguer <louison.diguer@isen-ouest.yncrea.fr>
2023-11-04 23:37:54 -0400

    fill

---
+++ js/fill.js
@@ -0,0 +1,19 @@
+function createForm(){
+    
+}
+
+export function printTypes(data) {
+    data.forEach(element => {
+      const listItem = $('<li>');
+      const checkbox = $('<input>').attr({
+        type: 'checkbox',
+        id: element['nom'],
+        name: element['nom'],
+        value: element['id']
+      });
+      const label = $('<label>').attr('for', element['nom']).text(element['nom']);
+    
+      listItem.append(checkbox, label);
+      $('#type_list').append(listItem);
+    });
+  }
