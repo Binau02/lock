1386d060a3e7a36e4d5bd8887c411ade5f58e82c
Binau02 <aubin.saunier@gmail.com>
2023-11-05 08:14:35 -0500

    oui

--- php/api.php
+++ php/api.php
@@ -1,10 +1,8 @@
 <?php
-echo "coucou";
-$test = [1, 2, 3];
-var_dump($test);
+echo "coucou<br>";
+var_dump($_POST);
 if (!empty($_POST['data'])) {
   $data = $_POST['data'];
-  var_dump($_POST);
   // $fname = ; //generates random name
 
   // $file = fopen("upload/" . $fname, 'w'); //creates new file
