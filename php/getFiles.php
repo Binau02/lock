5184839e630860bb80d26e378c36399af1cd8266
Binau02 <aubin.saunier@gmail.com>
2023-11-05 08:49:24 -0500

    qzdf

--- php/getFiles.php
+++ php/getFiles.php
@@ -1,7 +1,12 @@
 <?php
 
-$data = scandir("../saves/Philippe/template");
-$data = range(2, count($data)-1, 2);
+$temp = scandir("../saves/Philippe/template");
+$temp2 = range(2, count($temp)-1, 2);
+
+$data = [];
+for ($i = 0; $i < count($temp2); $i++) {
+  array_push($data, $temp[$temp2[$i]]);
+}
 
 
 
