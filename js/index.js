77072a3b4568c2a4323c47aaff79155d3324a3f8
Alexandre Radin <alexandre.radin@isen-ouest.yncrea.fr>
2023-11-05 01:40:59 -0500

    popup

--- js/index.js
+++ js/index.js
@@ -1,32 +1,50 @@
-
-function loadFileNames(dir) {
-    return new Promise((resolve, reject) => {
-        try {
-            var fileNames = new Array();
-            $.ajax({
-                url: dir,
-                success: function (data) {
-                    for(var i = 1; i < $(data).find('li span.name').length; i++){
-                        var elem = $(data).find('li span.name')[i];
-                        fileNames.push(elem.innerHTML);
-                    }
-                    return resolve(fileNames);
-                }
-            });
-        } catch (ex) {
-            return reject(new Error(ex));
-        }
-    });
+var directory = "../saves/Philippe";
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', directory, false); // false for synchronous request
+xmlHttp.send(null);
+var ret = xmlHttp.responseText;
+var fileList = ret.split('\n');
+for (i = 0; i < fileList.length; i++) {
+    var fileinfo = fileList[i].split(' ');
+    console.log(fileinfo)
+    // if (fileinfo[0] == '201:') {
+    //     document.write(fileinfo[1] + "<br>");
+    //     document.write('<img src="' + directory + fileinfo[1] + '"/>');
+    // }
 }
 
-loadFileNames('../saves/Philippe')
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            alert('Files could not be loaded. please check console for details');
-            console.error(error);
-        });
+
+// function loadFileNames(dir) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             var fileNames = new Array();
+//             $.ajax({
+//                 url: dir,
+//                 success: function (data) {
+//                     console.log($(data));
+//                     console.log((data));
+//                     console.log(data.getElementByTagName("a"));
+//                     for(var i = 1; i < $(data).find('[html_elem]').length; i++){
+//                         var elem = $(data).find('[html_elem]')[i];
+//                         fileNames.push(elem.innerHTML);
+//                     }
+//                     return resolve(fileNames);
+//                 }
+//             });
+//         } catch (ex) {
+//             return reject(new Error(ex));
+//         }
+//     });
+// }
+
+// loadFileNames('../saves/Philippe')  
+//         .then((data) => {
+//             console.log(data);
+//         })
+//         .catch((error) => {
+//             alert('Files could not be loaded. please check console for details');
+//             console.error(error);
+//         });
 
 $ = function(id) {
     return document.getElementById(id);
