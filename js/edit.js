14da3318f7eee29d8e60c2aa26681543d9bef919
Binau02 <aubin.saunier@gmail.com>
2023-11-05 01:27:14 -0400

    dragg edit

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,19 @@
 content = document.getElementById("content");
 
+start_pos = [0, 0]
+
+showingRect = document.createElement("div")
+showingRect.id = "showing-rect"
+
+document.addEventListener("keypress", (e) => {
+  switch(e.key) {
+    case "t":
+    console.log( document.elementFromPoint(e.clientX, e.clientY) );
+    break;
+  }
+})
+
+
 line = document.createElement("div");
 line.className = "line"
 title = document.createElement("div");
@@ -19,3 +33,56 @@ mechanism.className = "editable Mechanism"
 mechanism.innerHTML = "Section mechanism"
 line.append(mechanism)
 content.append(line);
+
+$( function() {
+  $( ".editable" ).draggable({
+    drag: function(event, ui) {
+      // console.log(event);
+      elem = document.elementsFromPoint(event.pageX, event.pageY)[1];
+      if (elem.localName == "body") {
+
+      }
+      else if (elem.className != "line"){
+        rect = elem.getBoundingClientRect();
+        if ((event.pageX-rect.x) < rect.width/10) {
+          console.log("left")
+        }
+        if ((rect.width+rect.x-event.pageX) < rect.width/10) {
+          console.log("right")
+        }
+        if ((event.pageY-rect.y) < rect.height/20) {
+          console.log("top")
+        }
+        if ((rect.height+rect.y-event.pageY) < rect.height/20) {
+          console.log("bottom")
+        }
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+// const swappable = new Draggable.Swappable(
+// 	document.querySelectorAll('.editable'), {
+// 		draggable: '[swapItem]',
+// 		delay: 0,
+// 	}
+// )
+
+// swappable.on('drag:start', () => {
+// 	console.log('drag:start')
+// })
+// swappable.on('swappable:swapped', () => {
+// 	console.log('drag:swapped')
+// })
+// swappable.on('drag:stop', () => {
+// 	console.log('drag:stop')
+// })
+// swappable.on('drag:move', () => {
+// 	console.log('drag:move')
+// })
