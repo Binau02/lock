9725ba79900a19008f737d02e50f87e25364d8cb
Binau02 <aubin.saunier@gmail.com>
2023-11-05 10:18:56 -0500

    qef

--- js/fill.js
+++ js/fill.js
@@ -416,10 +416,17 @@ function getData(){
     console.log(data);
 }
 
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+  }
+
 function createHtml() {
     content = document.getElementById("mainForm");
-    // content.innerHTML = ""
-    // console.log(Html)
-    content.innerHtml = Html
+    div = createElementFromHTML(Html)
+    console.log(div);
 
 }
