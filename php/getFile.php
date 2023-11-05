6ba4addb3346dda46f1f1bc0e47c39ef47b960a5
Binau02 <aubin.saunier@gmail.com>
2023-11-05 08:55:23 -0500

    remove get

--- php/getFile.php
+++ php/getFile.php
@@ -3,12 +3,12 @@
 // var_dump($_POST);
 
 $fname = $_POST["name"];
-$fname = $_GET["name"];
+// $fname = $_GET["name"];
 // var_dump($_GET["name"]);
 $data = file_get_contents("../saves/Philippe/template/" . $fname);
 
 
-if (isset($data)){
+if (isset($data)) {
   print_r(json_encode($data));
 }
 
