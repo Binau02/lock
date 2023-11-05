a44b87211515d0d7529a774102a67fc56acfdbbe
Binau02 <aubin.saunier@gmail.com>
2023-11-05 00:02:42 -0400

    oui

--- js/edit.js
+++ js/edit.js
@@ -2,3 +2,5 @@ content = document.getElementById("content");
 
 line = document.createElement("div");
 line.className = "line"
+title = document.createElement("div");
+title.className = "Editable Title"
