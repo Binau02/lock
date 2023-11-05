3d05246e0d216a2a06e6ebfcd22c8225206f577f
Louison-Diguer <louison.diguer@isen-ouest.yncrea.fr>
2023-11-05 01:03:06 -0400

    fill

--- js/fill.js
+++ js/fill.js
@@ -1,22 +1,61 @@
-$.ajax({
-    type: "GET",
-    url: "../js/data.js"
-})
-.done(function(data){
-    createFormFromJSON(data);
-})
-.fail(function(){
-    alert("ERROR : AJAXT REQUEST FAILED");
-});
-
 function createFormFromJSON(data){
+    const form = $("#mainForm");
     console.log(data);
-    data.forEach(element => {
-        console.log(element);
-    });
+    
+    for (var key in data){
+        if (key != "Procedure"){
+            const div = $('<div>').attr({
+                id: key
+            });
+            form.append(div);
+            for (var key1 in data[key]){
+                const subDiv = $('<div>').attr({
+                    id: key + key1
+                });
+                div.append(subDiv);
+                var result = getJSONElementByPath(key + key1);
+                if (result === ""){
+                    const inputText = $('<input>').attr({
+                        type: 'text',
+                        id: 'input' + key + key1
+                    });
+                    subDiv.append(inputText);
+                }
+                if (result === 0){
+                    const inputNum = $('<input>').attr({
+                        type: 'number',
+                        min: 0,
+                        id: 'input' + key + key1
+                    });
+                    subDiv.append(inputNum);
+                }
+            }
+        }
+    }
 }
 
-createFormFromJSON();
+function getJSONElementByPath(path){
+    let startIndex = 0;
+    let endIndex = 0;
+    let tabWord = [];
+    for (let i = 1; i < path.length; i++) {
+        if (/[A-Z]/.test(path[i])){
+            endIndex = i;
+            tabWord.push(path.substring(startIndex, endIndex));
+            startIndex = endIndex;
+        }        
+    }
+    tabWord.push(path.substring(startIndex, path.length));
+    console.log(tabWord);
+    var result = data[tabWord[0]];
+    for (let i = 1; i < tabWord.length; i++){
+        result = result[tabWord[i]];
+    }
+    return result;
+}
+
+createFormFromJSON(data);
+getJSONElementByPath('GeneralTitle');
 
 // export function printTypes(data) {
 //     data.forEach(element => {
